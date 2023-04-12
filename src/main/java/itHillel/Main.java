751cb482c7cd1685cbd13bc19e3f1319b119defd
Creepy.daffodil <Alexander26Peshkov@ukr.net>
2023-04-12 13:56:07 +0300

    Stream API

--- src/main/java/itHillel/Main.java
+++ src/main/java/itHillel/Main.java
@@ -1,5 +1,28 @@
-package itHillel;public class Main {
+package itHillel;
+
+import itHillel.product.Product;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("book", 50.0, true, LocalDate.now()));
+        products.add(new Product("book", 100.0, false, LocalDate.now().minusMonths(1)));
+        products.add(new Product("music", 10.0, false, LocalDate.now()));
+
+        String category = "book";
+        double price = 60.0;
+
+        try {
+            List<Product> filteredProducts = Product.getProductsByCategoryAndPrice(products, category, price);
+            System.out.println("Продукти з категорії " + category + " та ціною більшою за " + price);
+            for (Product product : filteredProducts) {
+                System.out.println(product.getType() + " " + product.getPrice() + " " + product.getDateAdded());
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/itHillel/product/Product.java
+++ src/main/java/itHillel/product/Product.java
@@ -1,2 +1,94 @@
-package itHillel.product;public class Product {
+package itHillel.product;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Product {
+    private String type;
+    private double price;
+    private boolean discountAvailable;
+    private LocalDate dateAdded;
+
+    public Product(String type, double price, boolean discountAvailable, LocalDate dateAdded) {
+        this.type = type;
+        this.price = price;
+        this.discountAvailable = discountAvailable;
+        this.dateAdded = dateAdded;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isDiscountAvailable() {
+        return discountAvailable;
+    }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
+
+    public static List<Product> getProductsByCategoryAndPrice(List<Product> products, String category, double price) {
+        List<Product> filteredProducts = products.stream()
+                .filter(p -> p.getType().equalsIgnoreCase(category) && p.getPrice() > price)
+                .collect(Collectors.toList());
+
+        if (filteredProducts.isEmpty()) {
+            throw new IllegalArgumentException("Продукт [категорія: " + category + "] не знайдено");
+        }
+
+        return filteredProducts;
+    }
+
+    public static List<Product> getDiscountedProductsByCategory(List<Product> products, String category) {
+        List<Product> filteredProducts = products.stream()
+                .filter(p -> p.getType().equalsIgnoreCase(category) && p.isDiscountAvailable())
+                .collect(Collectors.toList());
+
+        List<Product> discountedProducts = new ArrayList<>();
+        for (Product product : filteredProducts) {
+            double discountedPrice = product.getPrice() * 0.9;
+            discountedProducts.add(new Product(product.getType(), discountedPrice, false, product.getDateAdded()));
+        }
+
+        return discountedProducts;
+    }
+
+    public static Product getCheapestProductByCategory(List<Product> products, String category) {
+        List<Product> filteredProducts = products.stream()
+                .filter(p -> p.getType().equalsIgnoreCase(category))
+                .collect(Collectors.toList());
+
+        if (filteredProducts.isEmpty()) {
+            throw new IllegalArgumentException("Продукт [категорія: " + category + "] не знайдено");
+        }
+
+        return filteredProducts.stream()
+                .min(Comparator.comparingDouble(Product::getPrice))
+                .orElseThrow();
+    }
+
+    public static List<Product> getLastNProducts(List<Product> products, int n) {
+        int startIndex = Math.max(0, products.size() - n);
+        return products.subList(startIndex, products.size());
+    }
+
+    public static double getTotalCostOfBooksAddedThisYear(List<Product> products) {
+        return products.stream()
+                .filter(p -> p.getType().equalsIgnoreCase("book"))
+                .filter(p -> p.getPrice() <= 75)
+                .filter(p -> p.getDateAdded().getYear() == LocalDate.now().getYear())
+                .mapToDouble(Product::getPrice)
+                .sum();
+    }
+
+    public static Map<String, List<Product>> groupProductsByType(List<Product> products) {
+        return products.stream()
+                .collect(Collectors.groupingBy(Product::getType));
+    }
 }
